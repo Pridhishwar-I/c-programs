5c89e669f0da88748b3035f423da830b08c37677
Pridhishwar-I <277790707+Pridhishwar-I@users.noreply.github.com>
2026-08-18 15:34:43 +0530

    Add 2ndprog

---
+++ 2ndprog.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("enter a number : ");
+    scanf("%d",&a);
+    printf("enter a number : ");
+    scanf("%d",&b);
+    printf("the sum of two numbers is : %d",a+b);
+    return 0;
+}
