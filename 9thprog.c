65dad3771819c949d50078d81381c1604da4e776
Pridhishwar-I <277790707+Pridhishwar-I@users.noreply.github.com>
2026-08-19 16:00:56 +0530

    Add 9thprog.c

---
+++ 9thprog.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("enter a num  : ");
+    scanf("%d",&a);
+    printf("the size of the entered value is : %d",sizeof(a));
+    return 0;
+}
+    
+    
+
+
