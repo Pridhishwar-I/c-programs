79486c93f6b49aafbbe94ca2ef3008f152fdd664
Pridhishwar-I <277790707+Pridhishwar-I@users.noreply.github.com>
2026-08-19 11:33:18 +0530

    Add 8thprog.c

---
+++ 8thprog.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    char a;
+    printf("enter a character to find its ascii value : ");
+    scanf("%c",&a);
+    printf("the ascii value of the entered char is : %d",a);
+    return 0;
+}
